f272e157d1ecec7ac628ab1013cd836375e07aa2
N.J. Childs <sirbeerus@gmail.com>
2022-06-20 20:38:50 -0400

    Successful Post Request

--- haskell_API_sandbox_Main.hs
+++ haskell_API_sandbox_Main.hs
@@ -1,10 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-} 
+
 module Main where
 
 import Network.Wreq
+import Data.Text (Text)
+import Data.Aeson
+import GHC.Generics
+
+data TranslateRequest = TranslateRequest {
+ q :: Text,
+ source :: Text,
+ target :: Text,
+ format :: Text
+ }
+ deriving (Generic,Show)
+
+instance ToJSON TranslateRequest
 
 main :: IO ()
 main = do
- response <- get "https://libretranslate.com/languages"
+ response <- post "https://translate.argosopentech.com/translate" (toJSON (TranslateRequest {
+ q = "Poopy Haskell Programmer -Bob Bobo",
+ source = "en",
+ target = "es",
+ format = "text"
+ }))  
  print response 
 
 
