99009456ceca880d81db2e404c9bf293fb7b3461
N.J. Childs <sirbeerus@gmail.com>
2022-06-22 17:48:06 -0400

    multiple api calls

--- haskell_API_sandbox_Main.hs
+++ haskell_API_sandbox_Main.hs
@@ -10,6 +10,8 @@ import GHC.Generics
 import Language.Haskell.TH
 import Control.Lens
 import qualified Data.Text.IO as T
+import Control.Monad (forM_)
+ 
 
 data TranslateRequest = TranslateRequest {
  q :: Text,
@@ -26,21 +28,45 @@ instance ToJSON TranslateRequest
 data TranslateResponse = TranslateResponse {
  translatedText :: Text
  }
- deriving (Generic)
+ deriving (Generic,Show)
 
 instance FromJSON TranslateResponse 
  
 
 
+data Language = Language {
+ code :: Text,
+ name :: Text
+ }
+ deriving (Generic,Show) 
+
+instance FromJSON Language
+
+
 main :: IO ()
 main = do
+ languages <- getLanguages
+ forM_ languages $ \language -> do
+ result <- translateText "en" "Super duper poopy Haskell programmer." (code language)
+ T.putStrLn ((name language) <> ": " <> result)
+
+getLanguages :: IO [Language]
+getLanguages = do
+ response <- asJSON =<< get "https://translate.argosopentech.com/languages"
+ pure (response ^. responseBody)
+
+translateText :: Text -> Text -> Text -> IO Text
+translateText sourceLanguage text targetLanguage = do
  response <- asJSON =<< post "https://translate.argosopentech.com/translate" (toJSON (TranslateRequest {
- q = "Poopy Haskell Programmer -Bob Bobo",
- source = "en",
- target = "ru",
- format = "Text"
+  q = text,
+  source = sourceLanguage,
+  target = targetLanguage,
+  format = "text"
  }))
- T.putStrLn (translatedText (response ^. responseBody )) 
+ pure (translatedText (response ^. responseBody))
+
+
+
 
 
 
